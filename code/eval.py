65d2c5cc70a06a4cce226f0e6a3ce23770d279a7
Luka Družijanić <lukadruzijanic00@gmail.com>
2022-04-13 19:09:51 +0200

    Changed Conv-F2F.pt to hold only weights instead of the entire model

--- code/eval.py
+++ code/eval.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 from models.sci import ScaleInvariantModel
 from models.resnet.resnet_relu_noskip import resnet18
+from models.convf2f.conv_f2f import ConvF2F
 from datasets.cityscapes_halfres_ground_truth_dataset import CityscapesHalfresGroundTruthDataset
 from util import mIoU
 
@@ -68,39 +69,41 @@ class F2F(Model):
 		preds = torch.argmax(logits, 1).squeeze().cpu().numpy()
 		return preds
 
-class ConvF2F(F2F):
+class Conv_F2F(F2F):
 	"""
-	A convolutional F2F model consisting of 
+	A simple convolutional F2F model.
 	"""
 	def name(self) -> str:
 		return "Conv-F2F"
 
 	def F2Fmodel(self) -> torch.nn.Module:
-		return torch.load("../weights/Conv-F2F.pt").to("cuda")
-		
+		model = ConvF2F().to("cuda")
+		model.eval()
+		model.load_state_dict(torch.load("../weights/Conv-F2F.pt"))
+		return model
 
 if __name__ == '__main__':
 	dataset = CityscapesHalfresGroundTruthDataset(num_past=4)
 	print(f"Dataset contains {len(dataset)} items")
 
 	sys.stdout = open(os.devnull, 'w') # disable printing
-	models = [ConvF2F()]
+	models: list[Model] = [Oracle(), Conv_F2F()]
 	sys.stdout = sys.__stdout__ # enable printing
 
-	
+	#all_classes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 	moving_objects_classes = [0,1,2,4,5,7,8,10,11,13]
 
 	for model in models:
 		print(f"Evaluating {model.name()}...")
+
 		total_miou = 0
 		count = 0
 		for past_features, future_features, ground_truth in dataset:
-			print(count)
 			past_features, future_features = past_features.to("cuda"), future_features.to("cuda")
 			prediction = model.forecast(past_features, future_features)
 			total_miou += mIoU(prediction, ground_truth, moving_objects_classes)
 			count += 1
 		miou = total_miou / count
-		print(f"\nmIoU: {miou}")
+		print("\tmIoU-MO: %.3f" % miou)
 
 	
