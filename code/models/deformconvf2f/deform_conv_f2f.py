994e2df943734a07db566d2efaedc4bdecc44689
Luka Družijanić <lukadruzijanic00@gmail.com>
2022-05-06 17:03:47 +0200

    implemented deform conv f2f, set to train

--- code/models/deformconvf2f/deform_conv_f2f.py
+++ code/models/deformconvf2f/deform_conv_f2f.py
@@ -5,25 +5,28 @@ class DeformConvF2F(nn.Module):
 
 	def __init__(self, output_channels=128, num_past=4, layers=8):
 		super(DeformConvF2F, self).__init__()
-		# input (512, 16, 32)
-
-		self.layers = nn.ModuleList()
-
-		self.layers.append(ops.DeformConv2d(in_channels = num_past * output_channels, out_channels=256, kernel_size=1, padding=0))
-		self.layers.append(nn.ReLU())
-		# (256, 16, 32)
 		
-		self.layers.append(ops.DeformConv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1))
-		self.layers.append(nn.ReLU())
-		# (128, 16, 32)
+		self.conv1 = nn.Conv2d(in_channels = num_past*output_channels, out_channels=2*output_channels, kernel_size=1, padding=0)
+		self.relu1 = nn.ReLU()
 
-		for i in range(layers-3):		
-			self.layers.append(ops.DeformConv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1))
-			self.layers.append(nn.ReLU())
-			# (128, 16, 32)
+		self.conv2 = nn.Conv2d(in_channels=2*output_channels, out_channels=output_channels, kernel_size=3, padding=1)
+		self.relu2 = nn.ReLU()
 
-		self.layers.append(ops.DeformConv2d(in_channels=128, out_channels = output_channels, kernel_size=3, padding=1))
-		self.layers.append(nn.ReLU())
+		self.deform_convs = nn.ModuleList()
+		self.offsets = nn.ModuleList()
+		self.relus = nn.ModuleList()
+		for i in range(layers-4):
+			deform_kernel_size = 3
+			deform_padding = 1
+			self.deform_convs.append(ops.DeformConv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=deform_kernel_size, padding=deform_padding))
+			self.offsets.append(nn.Conv2d(in_channels=output_channels, out_channels=2*deform_kernel_size*deform_kernel_size, kernel_size=deform_kernel_size, padding=deform_padding))
+			self.relus.append(nn.ReLU())
+
+		self.conv3 = nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=3, padding=1)
+		self.relu3 = nn.ReLU()
+
+		self.conv4 = nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=3, padding=1)  
+		self.relu4 = nn.ReLU()
 
 		self.reset_parameters()
 
@@ -32,8 +35,20 @@ class DeformConvF2F(nn.Module):
 			if isinstance(m, nn.Conv2d):
 				nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
 				nn.init.constant_(m.bias, 0)
+		for m in self.offsets:
+			nn.init.constant_(m.weight, 0)
+			nn.init.constant_(m.bias, 0)
 
 	def forward(self, x):
-		for layer in self.layers:
-			x = layer.forward(x)
+		x = self.relu1.forward(self.conv1.forward(x))
+		x = self.relu2.forward(self.conv2.forward(x))
+
+		for i in range(len(self.deform_convs)):
+			offsets = self.offsets[i].forward(x)
+			x = self.deform_convs[i].forward(x, offsets)
+			x = self.relus[i].forward(x)
+
+		x = self.relu3.forward(self.conv3.forward(x))
+		x = self.relu4.forward(self.conv4.forward(x))
+
 		return x
