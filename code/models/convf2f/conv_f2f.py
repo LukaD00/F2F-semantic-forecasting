57c3ca0cc919221ae02f2f2ae6ab5d634478ba67
Luka Družijanić <lukadruzijanic00@gmail.com>
2022-05-10 15:31:48 +0200

    Clean up ConvF2F

--- code/models/convf2f/conv_f2f.py
+++ code/models/convf2f/conv_f2f.py
@@ -4,25 +4,18 @@ class ConvF2F(nn.Module):
 
 	def __init__(self, output_channels=128, num_past=4, layers=8):
 		super(ConvF2F, self).__init__()
-		# input (512, 16, 32)
 
 		self.layers = nn.ModuleList()
 
-		self.layers.append(nn.Conv2d(in_channels = num_past * output_channels, out_channels=256, kernel_size=1, padding=0))
+		self.layers.append(nn.Conv2d(in_channels=num_past*output_channels, out_channels=2*output_channels, kernel_size=1, padding=0))
 		self.layers.append(nn.ReLU())
-		# (256, 16, 32)
 		
-		self.layers.append(nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1))
+		self.layers.append(nn.Conv2d(in_channels=2*output_channels, out_channels=output_channels, kernel_size=3, padding=1))
 		self.layers.append(nn.ReLU())
-		# (128, 16, 32)
 
-		for i in range(layers-3):		
-			self.layers.append(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1))
+		for i in range(layers-2):		
+			self.layers.append(nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=3, padding=1))
 			self.layers.append(nn.ReLU())
-			# (128, 16, 32)
-
-		self.layers.append(nn.Conv2d(in_channels=128, out_channels = output_channels, kernel_size=3, padding=1))
-		self.layers.append(nn.ReLU())
 
 		self.reset_parameters()
 
