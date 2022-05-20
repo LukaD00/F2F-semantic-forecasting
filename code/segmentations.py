2b989896959a5dcd747f4f930e43f3f1732c3433
Luka Družijanić <lukadruzijanic00@gmail.com>
2022-05-20 14:52:51 +0200

    segmentations.py now saves in folder inside output, and now also saves original images

--- code/segmentations.py
+++ code/segmentations.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import torch
 import numpy as np
+import os
 
 from models.convf2f.conv_f2f import ConvF2F
 from models.dilatedf2f.dilated_f2f import DilatedF2F
@@ -38,50 +39,69 @@ if __name__ == '__main__':
 
 	folder = "val"
 	city = "frankfurt"
-	seq = "000001"
+	seq = "000000"
 	city_seq = city + "_" + seq
-	tag = 34047
-
-	past0_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{str(tag-12).zfill(6)}_leftImg8bit.npy"
-	past1_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{str(tag-9).zfill(6)}_leftImg8bit.npy"
-	past2_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{str(tag-6).zfill(6)}_leftImg8bit.npy"
-	past3_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{str(tag-3).zfill(6)}_leftImg8bit.npy"
-	future_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{str(tag).zfill(6)}_leftImg8bit.npy"
-	future_gt_path = f"../cityscapes-gt/{folder}/{city_seq}_{str(tag).zfill(6)}_gtFine_labelTrainIds.png"
-	original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{str(tag).zfill(6)}_leftImg8bit.png"
-
-	past0 = np.load(past0_path)
-	past1 = np.load(past1_path)
-	past2 = np.load(past2_path)
-	past3 = np.load(past3_path)
-	past_features = torch.from_numpy(np.vstack([past0, past1, past2, past3]))
-	future_features = torch.from_numpy(np.load(future_path))
+	tag = 8451
+	num_past = 4
 
 	colormap = create_cityscapes_label_colormap()
-
 	models: list[Model] = [
-		#Oracle(),
-		#F2F(ConvF2F(layers=8), "ConvF2F-8"),
-		#F2F(DilatedF2F(layers=8), "DilatedF2F-8"),
-		F2F(DeformF2F(layers=8), "DeformF2F-8")
+		Oracle(),
+		F2F(ConvF2F(layers=8), "ConvF2F-8"),
+		F2F(DilatedF2F(layers=8), "DilatedF2F-8"),
+		#F2F(DeformF2F(layers=8), "DeformF2F-8")
 	]
 
+	future_tag = str(tag).zfill(6)
+	save_path = f"../output/{city}_{seq}_{future_tag}/"
+	if (not os.path.exists(save_path)): os.mkdir(save_path)
+
+	# LOAD FUTURE FEATURES
+	future_feature_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{future_tag}_leftImg8bit.npy"
+	future_features = torch.from_numpy(np.load(future_feature_path))
+
+	# LOAD PAST FEATURES
+	past_features = []
+	for i in range(1,num_past+1):
+		past_tag = str(tag-i*3).zfill(6)
+		past_feature_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{past_tag}_leftImg8bit.npy"
+		past_feature = np.load(past_feature_path)
+		past_features.append(past_feature)
+	past_features = torch.from_numpy(np.vstack(past_features[::-1]))
+
+	# (FUTURE_FEATURES, PAST_FEATURES) => MODEL PREDICTIONS
 	for model in models:
 		preds = model.forecast(past_features, future_features)
 		preds_colored = colormap[preds]
 		image = Image.fromarray(preds_colored)
-		path = f"../output/{city_seq}_{str(tag).zfill(6)}_{model.getName()}.png"
-		image.save(path)
-		print(f"{model.getName()} prediction saved to {path}")
+		image.save(save_path + f"{model.getName()}.png")
+		print(f"{model.getName()} prediction saved to {save_path}")
 
+	# FUTURE GROUND TRUTH LABELING
+	future_gt_path = f"../cityscapes-gt/{folder}/{city_seq}_{future_tag}_gtFine_labelTrainIds.png"
 	ground_truth = np.array(Image.open(future_gt_path))
 	ground_truth_colored = colormap[ground_truth]
 	image = Image.fromarray(ground_truth_colored)
-	path = f"../output/{city_seq}_{str(tag).zfill(6)}_gt.png"
-	image.save(path)
-	print(f"Ground truth saved to {path}")
-
+	image.save(save_path + "GroundTruth.png")
+	print(f"Ground truth saved to {save_path}")
+	
+	# ORIGINAL FUTURE IMAGE
+	original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{future_tag}_leftImg8bit.png"
 	original = Image.open(original_path)
-	path = f"../output/{city_seq}_{str(tag).zfill(6)}_original.png"
-	original.save(path)
-	print(f"Original image saved to {path}")
+	original.save(save_path + "Future.png")
+	print(f"Original future image saved to {save_path}")
+
+	# ORIGINAL PAST IMAGES
+	for i in range(1,num_past+1):
+		past_tag = str(tag-i*3).zfill(6)
+		past_original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{past_tag}_leftImg8bit.png"
+		past_original = Image.open(past_original_path)
+		past_original.save(save_path + f"Past{i}.png")
+		print(f"Original past{i} image saved to {save_path}")
+	
+
+	
+
+	
+
+	
