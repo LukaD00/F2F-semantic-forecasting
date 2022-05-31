c038e1073c4fa0bade529af10a785f0c116cbd31
Luka Družijanić <lukadruzijanic00@gmail.com>
2022-06-01 00:06:26 +0200

    updated segmentations.py and gradients.py to work with multiple models and images at once, saved outputs

--- code/gradients.py
+++ code/gradients.py
@@ -1,13 +1,14 @@
 import torch
 from torch.nn import functional as F
-from torchvision import transforms
 import numpy as np
 from PIL import Image
 from models.sci import ScaleInvariantModel
 from models.resnet.resnet_relu_noskip import resnet18
-from colormap import colormap
-from models.model import F2F
+from models.model import F2F, Model
 from models.deformf2f.deform_f2f import DeformF2F
+from models.convf2f.conv_f2f import ConvF2F
+from models.dilatedf2f.dilated_f2f import DilatedF2F
+from colormap import colormap
 import os
 
 def load_features(feature_path : str) -> torch.Tensor:
@@ -51,87 +52,116 @@ def generate_pixel_neighbourhood(pixel : tuple[int,int], size : int) -> list[tup
 
 if __name__=="__main__":
 
-	folder = "val"
-	city = "frankfurt"
-	seq = "000000"
-	city_seq = city + "_" + seq
-	tag = 8451
-	num_past = 4
+	midterm = True
 
-	model = F2F(DeformF2F(layers=8), "DeformF2F-8")
+	images = [
+		("val", "frankfurt", "000000", 8451, 4, (564, 1644)),
+		("val", "frankfurt", "000001", 32556, 4, (405, 2013)),
+		("val", "frankfurt", "000001", 7285, 4, (599, 31)),
+		("val", "frankfurt", "000000", 294, 4, (448, 1725))
+	]
 
-	future_tag = str(tag).zfill(6)
-	save_path = f"../output/gradients/{city}_{seq}_{future_tag}/"
-	if (not os.path.exists(save_path)): os.mkdir(save_path)
+	models: list[Model] = [
+		#F2F(ConvF2F(layers=8), "ConvF2F-8-3"),
+		#F2F(DilatedF2F(layers=8), "DilatedF2F-8-3-24"),
+		#F2F(DeformF2F(layers=8), "DeformF2F-8-3-24")
 
-	chosen_pixel = (564, 1644)
-	chosen_pixel_color = [0, 255, 0]
-	gradient_pixel_color = [255, 0, 0]
-	pixels = generate_pixel_neighbourhood(chosen_pixel, 5)
-	k = 3000  # how many pixels in X_t will be marked
+		F2F(ConvF2F(layers=8), "ConvF2F-8-M-24"),
+		F2F(DilatedF2F(layers=8), "DilatedF2F-8-M"),
+		F2F(DeformF2F(layers=8), "DeformF2F-8-M")
+	]
 
+	for folder, city, seq, tag, num_past, chosen_pixel in images:
 
-	# LOAD FUTURE FEATURES
-	future_feature_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{future_tag}_leftImg8bit.png"
-	future_features, future_img_tensor = load_features(future_feature_path)
+		for model in models:
 
-	# LOAD PAST FEATURES
-	past_features_list = []
-	past_tensors_list = []
-	for i in range(1,num_past+1):
-		past_tag = str(tag-i*3).zfill(6)
-		past_feature_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{past_tag}_leftImg8bit.png"
-		past_feature, past_tensor = load_features(past_feature_path)
-		past_features_list.append(past_feature.squeeze(0))
-		past_tensors_list.append(past_tensor)
-	past_features_list = past_features_list[::-1]
-	past_tensors_list = past_tensors_list[::-1]
-	past_features = torch.vstack(past_features_list)
+			city_seq = city + "_" + seq
 
-	# PREDICT NEW FEATURES
-	logits = model.forecastLogits(past_features, future_features)
-	preds = torch.argmax(logits, 1).squeeze()
-	preds_colored = colormap[preds]
-	for x,y in pixels:
-		preds_colored[x,y] = chosen_pixel_color
-	image = Image.fromarray(preds_colored)
-	image.save(save_path + f"{model.getName()}.png")
-	print(f"{model.getName()} prediction saved to {save_path}")
+			future_tag = str(tag).zfill(6)
 
-	# ORIGINAL FUTURE IMAGE
-	original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{future_tag}_leftImg8bit.png"
-	original = Image.open(original_path)
-	for x,y in pixels:
-		original.putpixel((y,x), tuple(chosen_pixel_color))
-	original.save(save_path + "Future.png")
-	print(f"Original future image saved to {save_path}")
+			if midterm:
+				save_path = f"../output/gradients/{city}_{seq}_{future_tag}-midterm/"
+			else:
+				save_path = f"../output/gradients/{city}_{seq}_{future_tag}/"
+			if (not os.path.exists(save_path)): os.mkdir(save_path)
+
+			save_path += f"{model.getName()}/"
+			if (not os.path.exists(save_path)): os.mkdir(save_path)
+
+			chosen_pixel_color = [0, 255, 0]
+			gradient_pixel_color = [255, 0, 0]
+			chosen_pixel_neighbourhood = generate_pixel_neighbourhood(chosen_pixel, 8)
+			k = 3000  # how many pixels in X_t will be marked
 
 
-	# GRADIENTS
-	logits = logits.squeeze()
-	logits = logits.permute(1,2,0)
-	chosen_logits = logits[chosen_pixel[0], chosen_pixel[1]]
-	logmaxsoftmax = torch.log(torch.max(F.softmax(chosen_logits, dim=0))) # TODO: check dim
-	logmaxsoftmax.backward() 
+			# LOAD FUTURE FEATURES
+			future_feature_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{future_tag}_leftImg8bit.png"
+			future_features, future_img_tensor = load_features(future_feature_path)
 
-	past_last = past_tensors_list[-1]
-	sorted, indices = torch.abs(torch.sum(past_last.grad, dim=0)).flatten().sort()
-	threshold = sorted[-(k-1)]
+			# LOAD PAST FEATURES
+			past_features_list = []
+			past_tensors_list = []
+			for i in range(1,num_past+1):
+				if midterm:
+					past_tag = str(tag-6-i*3).zfill(6)
+				else:
+					past_tag = str(tag-i*3).zfill(6)
+				past_feature_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{past_tag}_leftImg8bit.png"
+				past_feature, past_tensor = load_features(past_feature_path)
+				past_features_list.append(past_feature.squeeze(0))
+				past_tensors_list.append(past_tensor)
+			past_features_list = past_features_list[::-1]
+			past_tensors_list = past_tensors_list[::-1]
+			past_features = torch.vstack(past_features_list)
 
-	for i in range(len(past_tensors_list)):
-		
-		past_tensor = past_tensors_list[i].clone().detach()
-		past_tensor = past_tensor.permute(1,2,0)
-		grads = torch.abs(torch.sum(past_tensors_list[i].grad, dim=0))
+	
+			# PREDICT NEW FEATURES
+			logits = model.forecastLogits(past_features, future_features)
+			preds = torch.argmax(logits, 1).squeeze()
+			preds_colored = colormap[preds]
+			for x,y in chosen_pixel_neighbourhood:
+				preds_colored[x,y] = chosen_pixel_color
+			image = Image.fromarray(preds_colored)
+			image.save(save_path + f"{model.getName()}.png")
+			print(f"{model.getName()} prediction saved to {save_path}")
 
-		pixels = grads>threshold
-		indices = pixels.nonzero()
-		for x,y in indices:
-			for x1, y1 in generate_pixel_neighbourhood((x,y), 2):
-				past_tensor[x1][y1] = torch.tensor(gradient_pixel_color).double()
-		#past_tensor[grads>threshold] = torch.tensor(gradient_pixel_color).double()
-		
-		past_tensor_np = past_tensor.int().numpy()
-		img = Image.fromarray(past_tensor_np.astype(np.uint8))
-		img.save(save_path + f"Past{i}.png")
-		print(f"Original past{i} image saved to {save_path}")
+			# ORIGINAL FUTURE IMAGE
+			original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{future_tag}_leftImg8bit.png"
+			original = Image.open(original_path)
+			for x,y in chosen_pixel_neighbourhood:
+				original.putpixel((y,x), tuple(chosen_pixel_color))
+			original.save(save_path + "Future.png")
+			print(f"Original future image saved to {save_path}")
+
+
+			# GRADIENTS
+			logits = logits.squeeze()
+			logits = logits.permute(1,2,0)
+			chosen_logits = logits[chosen_pixel[0], chosen_pixel[1]]
+			logmaxsoftmax = torch.log(torch.max(F.softmax(chosen_logits, dim=0))) # TODO: check dim
+			logmaxsoftmax.backward() 
+
+			past_last = past_tensors_list[-1]
+			sorted, indices = torch.abs(torch.sum(past_last.grad, dim=0)).flatten().sort()
+			threshold = sorted[-(k-1)]
+
+			for i in range(len(past_tensors_list)):
+				
+				past_tensor = past_tensors_list[i].clone().detach()
+				past_tensor = past_tensor.permute(1,2,0)
+				grads = torch.abs(torch.sum(past_tensors_list[i].grad, dim=0))
+
+				pixels = grads>threshold
+				indices = pixels.nonzero()
+				for x,y in indices:
+					for x1, y1 in generate_pixel_neighbourhood((x,y), 2):
+						past_tensor[x1][y1] = torch.tensor(gradient_pixel_color).double()
+				for x,y in chosen_pixel_neighbourhood:
+					past_tensor[x][y] = torch.tensor(chosen_pixel_color).double()
+				
+				#past_tensor[grads>threshold] = torch.tensor(gradient_pixel_color).double()
+				
+				past_tensor_np = past_tensor.int().numpy()
+				img = Image.fromarray(past_tensor_np.astype(np.uint8))
+				img.save(save_path + f"Past{i}.png")
+				print(f"Original past{i} image saved to {save_path}")
--- code/segmentations.py
+++ code/segmentations.py
@@ -11,67 +11,85 @@ from colormap import colormap
 
 if __name__ == '__main__':
 
-	folder = "val"
-	city = "frankfurt"
-	seq = "000000"
-	city_seq = city + "_" + seq
-	tag = 10351
-	num_past = 4
+	midterm = True
 
+	images = [
+		("val", "frankfurt", "000000", 294, 4),
+		("val", "frankfurt", "000000", 10351, 4),
+		("val", "frankfurt", "000001", 25921, 4),
+		("val", "frankfurt", "000001", 25713, 4)
+	]
 	
 	models: list[Model] = [
+		#Oracle(),
+		#F2F(ConvF2F(layers=8), "ConvF2F-8-3"),
+		#F2F(DilatedF2F(layers=8), "DilatedF2F-8-3-24"),
+		#F2F(DeformF2F(layers=8), "DeformF2F-8-3-24")
+
 		Oracle(),
-		#2F(ConvF2F(layers=8), "ConvF2F-8"),
-		#F2F(DilatedF2F(layers=8), "DilatedF2F-8"),
-		F2F(DeformF2F(layers=8), "DeformF2F-8")
+		F2F(ConvF2F(layers=8), "ConvF2F-8-M-24"),
+		F2F(DilatedF2F(layers=8), "DilatedF2F-8-M"),
+		F2F(DeformF2F(layers=8), "DeformF2F-8-M")
 	]
 
-	future_tag = str(tag).zfill(6)
-	save_path = f"../output/segmentations/{city}_{seq}_{future_tag}/"
-	if (not os.path.exists(save_path)): os.mkdir(save_path)
+	for folder, city, seq, tag, num_past in images:
+		city_seq = city + "_" + seq
 
-	# LOAD FUTURE FEATURES
-	future_feature_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{future_tag}_leftImg8bit.npy"
-	future_features = torch.from_numpy(np.load(future_feature_path))
+		future_tag = str(tag).zfill(6)
+		if midterm:
+			save_path = f"../output/segmentations/{city}_{seq}_{future_tag}-midterm/"
+		else:
+			save_path = f"../output/segmentations/{city}_{seq}_{future_tag}/"
+		if (not os.path.exists(save_path)): os.mkdir(save_path)
 
-	# LOAD PAST FEATURES
-	past_features = []
-	for i in range(1,num_past+1):
-		past_tag = str(tag-i*3).zfill(6)
-		past_feature_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{past_tag}_leftImg8bit.npy"
-		past_feature = np.load(past_feature_path)
-		past_features.append(past_feature)
-	past_features = torch.from_numpy(np.vstack(past_features[::-1]))
+		# LOAD FUTURE FEATURES
+		future_feature_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{future_tag}_leftImg8bit.npy"
+		future_features = torch.from_numpy(np.load(future_feature_path))
 
-	# (FUTURE_FEATURES, PAST_FEATURES) => MODEL PREDICTIONS
-	for model in models:
-		preds = model.forecast(past_features, future_features)
-		preds_colored = colormap[preds]
-		image = Image.fromarray(preds_colored)
-		image.save(save_path + f"{model.getName()}.png")
-		print(f"{model.getName()} prediction saved to {save_path}")
+		# LOAD PAST FEATURES
+		past_features = []
+		for i in range(1,num_past+1):
+			if midterm:
+				past_tag = str(tag-6-i*3).zfill(6)
+			else:
+				past_tag = str(tag-i*3).zfill(6)
+			past_feature_path = f"../cityscapes_halfres_features_r18/{folder}/{city_seq}_{past_tag}_leftImg8bit.npy"
+			past_feature = np.load(past_feature_path)
+			past_features.append(past_feature)
+		past_features = torch.from_numpy(np.vstack(past_features[::-1]))
 
-	# FUTURE GROUND TRUTH LABELING
-	future_gt_path = f"../cityscapes-gt/{folder}/{city_seq}_{future_tag}_gtFine_labelTrainIds.png"
-	ground_truth = np.array(Image.open(future_gt_path))
-	ground_truth_colored = colormap[ground_truth]
-	image = Image.fromarray(ground_truth_colored)
-	image.save(save_path + "GroundTruth.png")
-	print(f"Ground truth saved to {save_path}")
-	
-	# ORIGINAL FUTURE IMAGE
-	original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{future_tag}_leftImg8bit.png"
-	original = Image.open(original_path)
-	original.save(save_path + "Future.png")
-	print(f"Original future image saved to {save_path}")
+		# (FUTURE_FEATURES, PAST_FEATURES) => MODEL PREDICTIONS
+		for model in models:
+			preds = model.forecast(past_features, future_features)
+			preds_colored = colormap[preds]
+			image = Image.fromarray(preds_colored)
+			image.save(save_path + f"{model.getName()}.png")
+			print(f"{model.getName()} prediction saved to {save_path}")
 
-	# ORIGINAL PAST IMAGES
-	for i in range(1,num_past+1):
-		past_tag = str(tag-i*3).zfill(6)
-		past_original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{past_tag}_leftImg8bit.png"
-		past_original = Image.open(past_original_path)
-		past_original.save(save_path + f"Past{i}.png")
-		print(f"Original past{i} image saved to {save_path}")
+		# FUTURE GROUND TRUTH LABELING
+		future_gt_path = f"../cityscapes-gt/{folder}/{city_seq}_{future_tag}_gtFine_labelTrainIds.png"
+		ground_truth = np.array(Image.open(future_gt_path))
+		ground_truth_colored = colormap[ground_truth]
+		image = Image.fromarray(ground_truth_colored)
+		image.save(save_path + "GroundTruth.png")
+		print(f"Ground truth saved to {save_path}")
+		
+		# ORIGINAL FUTURE IMAGE
+		original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{future_tag}_leftImg8bit.png"
+		original = Image.open(original_path)
+		original.save(save_path + "Future.png")
+		print(f"Original future image saved to {save_path}")
+
+		# ORIGINAL PAST IMAGES
+		for i in range(1,num_past+1):
+			if midterm:
+				past_tag = str(tag-6-i*3).zfill(6)
+			else:
+				past_tag = str(tag-i*3).zfill(6)
+			past_original_path = f"../cityscapes-original/{folder}/{city}/{city_seq}_{past_tag}_leftImg8bit.png"
+			past_original = Image.open(past_original_path)
+			past_original.save(save_path + f"Past{i}.png")
+			print(f"Original past{i} image saved to {save_path}")
 	
 
 	
